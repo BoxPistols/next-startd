0c21f520d2b2e1557141312505cfefc469d88cce
aito <BoxPistols@users.noreply.github.com>
2022-11-19 07:01:59 +0900

    feat(test tw color): test move

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ const headerStyle = css`
   background-color: #f9f9f9;
   background-image: url('http://placeimg.com/1920/640/any');
   background-repeat: no-repeat;
-  min-height: calc(100vh - 6rem);
+  // min-height: calc(100vh - 6rem);
   position: relative;
   z-index: 1;
 `;
@@ -19,27 +19,27 @@ const overLay = css`
   background: linear-gradient(-45deg, rgba(180, 43, 65, 0.6), rgba(45, 65, 188, 0.6));
   position: absolute;
   width: 100%;
-  height: 100%;
+  // height: 100%;
   top: 0;
   left: 0;
   right: 0;
-  z-index: 2;
+  z-index: -1;
 `;
 
 const Header = () => (
   // FIXME: color
   <header className={tw(headerStyle, `bg-gradient-to-r from-violet-500 to-fuchsia-500`)}>
-    <div className={tw(overLay)} />
     <div className={tw(`max-w-4xl mx-auto py-16 px-14 sm:px-6 lg:px-8`)}>
+      <div className={tw(overLay)} />
       <h1
         className={`${tw(
-          `font-sans font-bold text-4xl md:text-5xl lg:text-8xl text-center leading-snug text-gray-800`,
+          `font-sans font-bold text-4xl md:text-5xl lg:text-8xl text-center leading-snug text-gray-200`,
         )} hoge`}
       >
         Your website, beyond expectations
       </h1>
       <div className={tw(`max-w-xl mx-auto`)}>
-        <p className={tw(`mt-10 text-gray-500 text-center text-xl lg:text-3xl`)}>
+        <p className={tw(`mt-10 text-gray-200 text-center text-xl lg:text-3xl`)}>
           Make your website wonderful and build beyond your expectations.
         </p>
       </div>
